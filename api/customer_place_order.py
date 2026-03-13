908c8381d6fab71bf6bbfa75de010a2e6bc928fb
Naidu Bhanu Teja <bhanuteja18112001@gmail.com>
2026-03-13 14:01:12 +0530

    `Changed filter condition in customer_current_day_order_api function from order_date__date=today to status="pending"`

--- api/customer_place_order.py
+++ api/customer_place_order.py
@@ -91,7 +91,7 @@ def customer_current_day_order_api(request):
     today = timezone.localdate()
 
     orders = (
-        CustomerOrder.objects.filter(customer=customer, order_date__date=today)
+        CustomerOrder.objects.filter(customer=customer, status="pending")
         .prefetch_related("items__item")
         .order_by("-order_date")
     )
